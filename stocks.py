4e730a40532db9c580b786b2ef565fc065c8a771
davidzarud <56953753+davidzarud@users.noreply.github.com>
2024-05-12 01:54:46 +0300

    Update stocks.py

--- stocks.py
+++ stocks.py
@@ -3,6 +3,8 @@ import yfinance as yf
 from bs4 import BeautifulSoup
 import requests
 import logging
+import concurrent.futures
+import requests_cache 
 
 app = Flask(__name__)
 
@@ -13,6 +15,8 @@ logger = logging.getLogger(__name__)
 @app.route('/api/v1/stock-price', methods=['GET'])
 def get_stock_price_by_ticker():
     # Get the ticker name from the request
+    session = requests_cache.CachedSession('yfinance.cache')
+    session.headers['User-agent'] = 'bnhp-stock=app/1.0'
     ticker = request.args.get('ticker')
     
     if not ticker:
@@ -20,7 +24,7 @@ def get_stock_price_by_ticker():
     
     try:
         # Get data for the specified ticker
-        stock = yf.Ticker(ticker)
+        stock = yf.Ticker(ticker, session = session)
         
         # Fetching additional data
         info = stock.info
@@ -39,17 +43,60 @@ def get_stock_price_by_ticker():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
+
 @app.route('/api/v1/sp500-tickers', methods=['GET'])
+def get_top_50_tickers():
+    tickers_and_names = get_sp500_tickers_with_names()
+    
+    # Fetch market caps for tickers concurrently
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        market_caps = list(executor.map(get_market_cap, tickers_and_names.keys()))
+    
+    # Combine tickers, names, and market caps
+    tickers_names_market_caps = [(ticker, tickers_and_names[ticker], market_cap) for ticker, market_cap in zip(tickers_and_names.keys(), market_caps)]
+    
+    # Filter out tickers with None market cap values
+    valid_tickers_names_market_caps = [(ticker, name, market_cap) for ticker, name, market_cap in tickers_names_market_caps if market_cap is not None]
+    
+    # Sort valid tickers by market cap
+    sorted_tickers_names_market_caps = sorted(valid_tickers_names_market_caps, key=lambda x: x[2], reverse=True)
+    
+    # Get top 50 tickers and company names
+    top_50_tickers_names = [{'ticker': ticker, 'name': name} for ticker, name, _ in sorted_tickers_names_market_caps[:50]]
+    
+    return jsonify({'companies': top_50_tickers_names})
+
+def get_sp500_tickers_with_names():
+    url = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    table = soup.find('table', {'class': 'wikitable sortable'})
+    rows = table.findAll('tr')[1:]
+    tickers_and_names = {}
+    for row in rows:
+        cols = row.findAll('td')
+        ticker = cols[0].text.strip()
+        name = cols[1].text.strip()
+        tickers_and_names[ticker] = name
+    return tickers_and_names
+    
 def get_sp500_tickers():
     url = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     table = soup.find('table', {'class': 'wikitable sortable'})
-    tickers = []
-    for row in table.findAll('tr')[1:]:
-        ticker = row.findAll('td')[0].text.strip()
-        tickers.append(ticker)
-    return jsonify({'tickers': tickers})
+    tickers = [row.findAll('td')[0].text.strip() for row in table.findAll('tr')[1:]]
+    return tickers
+
+def get_market_cap(ticker):
+    try:
+        session = requests_cache.CachedSession('yfinance.cache')
+        session.headers['User-agent'] = 'bnhp-stock=app/1.0'
+        stock = yf.Ticker(ticker, session=session)
+        return stock.info.get('marketCap')
+    except Exception as e:
+        print(f"Error fetching market cap for {ticker}: {e}")
+        return None
 
 @app.route('/api/v1/sp500-stock-price', methods=['POST'])
 def get_sp_500_stock_price():
@@ -66,7 +113,9 @@ def get_sp_500_stock_price():
         tickers_str = " ".join(tickers)
         
         # Get data for all tickers at once
-        stocks = yf.Tickers(tickers_str)
+        session = requests_cache.CachedSession('yfinance.cache')
+        session.headers['User-agent'] = 'bnhp-stock=app/1.0'
+        stocks = yf.Tickers(tickers_str, session = session)
                 
         results = []
         
